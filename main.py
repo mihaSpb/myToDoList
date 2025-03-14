ef98501db8b1728a2332fb8b6e34e02d851a02f6
Mikhail <mihaspb2007@gmail.com>
2025-03-14 14:44:28 +0300

    Add homework ToDoList

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-import to_do_list
+from to_do_list import *
 
 
+def main():
+    todo = ToDoList()
+    todo.add_task("Сделать домашнее задание")
+    todo.add_task("Написать текст")
+    todo.add_task("Отправить задание на проверку")
+
+    todo.list_tasks()
+
+    print("\nЗадача выполнена 'Сделать домашнее задание'")
+    todo.complete_task("Сделать домашнее задание")
+    todo.list_tasks()
+
+    print("\nУдаляем выполненную задачу 'Сделать домашнее задание'")
+    todo.remove_task("Сделать домашнее задание")
+    todo.list_tasks()
+
+    print("\nОтмечаем несуществующую задачу 'Заказать воду':")
+    todo.complete_task("Заказать воду")
+
+    print("\nДобавляем существующую задачу 'Написать текст':")
+    todo.add_task("Написать текст")
+    todo.list_tasks()
+
+
+main()
--- to_do_list.py
+++ to_do_list.py
@@ -1,3 +1,36 @@
 class ToDoList:
     def __init__(self):
         self.tasks = {}
+
+
+    def add_task(self, task):
+        if task not in self.tasks:
+            self.tasks[task] = False
+        else:
+            print("Задача уже существует!")
+
+
+    def complete_task (self, task):
+        if task in self.tasks:
+            self.tasks[task] = True
+        else:
+            self.task_is_null()
+
+
+    def remove_task(self, task):
+        if task in self.tasks:
+            del self.tasks[task]
+        else:
+            self.task_is_null()
+
+
+    def list_tasks(self):
+        print(f"\nСписок задач:")
+
+        for task, completed in self.tasks.items():
+            status = "[V]" if completed else "[X]"
+            print(f"{status} {task}")
+
+
+    def task_is_null(self):
+        print(f"Задача отсутствует!")
